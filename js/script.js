ef2f954b8d1e215dbc44409222e9879d3d64e49f
unknown <isratesha022@gmail.com>
2024-10-03 16:33:19 +0600

    latest two section added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,39 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
 });
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3, // Show three items
+    spaceBetween: 30, // Add space between items
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  
+    breakpoints: {
+        768: {
+            slidesPerView: 2, // Show two items on medium screens
+            spaceBetween: 20,
+        },
+        480: {
+            slidesPerView: 1, // Show one item on small screens
+            spaceBetween: 10,
+        },
+    },
+});
+
+
+ 
